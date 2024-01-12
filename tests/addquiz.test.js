63703483adb0721ad00165396e7200a4fa97df94
sanjana650 <sanjugundeti@gmail.com>
2024-01-12 18:53:15 +0800

    completed add-quiz internal server error test case also

--- tests/addquiz.test.js
+++ tests/addquiz.test.js
@@ -1,7 +1,19 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const proxyquire = require('proxyquire');
 const { admin } = require('../firebaseAdmin.js');
 const { createQuizWithQuestions } = require('../utils/QuizzesUtil');
+
+// Mocking the entire firebaseAdmin module to simulate an internal server error
+const adminMockInternalError = {
+  firestore: () => ({
+    collection: () => ({
+      add: sinon.stub().throws(new Error('Internal Server Error')),
+    }),
+  }),
+};
+
+const { createQuizWithQuestions: createQuizWithQuestionsInternalError } = proxyquire('../utils/QuizzesUtil', { '../firebaseAdmin.js': { admin: adminMockInternalError } });
 
 describe('Testing Add Quiz Function', () => {
   let addStub;
@@ -9,7 +21,6 @@ describe('Testing Add Quiz Function', () => {
   beforeEach(() => {
     addStub = sinon.stub(admin.firestore().collection('quizzes'), 'add').resolves({ id: 'mockedQuizId' });
   });
-
 
   afterEach(() => {
     addStub.restore();
@@ -43,7 +54,6 @@ describe('Testing Add Quiz Function', () => {
 
     expect(res.statusCode).to.equal(201);
     expect(res.data.message).to.equal('Quiz created successfully.');
-
   });
 
 
@@ -151,5 +161,35 @@ describe('Testing Add Quiz Function', () => {
     expect(res.data.message).to.equal('Invalid correct option provided for creating quiz.');
   });
 
+  it('should handle internal server error', async () => {
+    const newQuiz = {
+      quizTitle: 'Test Quiz123',
+      quizCourse: 'Test course',
+      questions: [
+        {
+          questionTitle: 'Test Question',
+          options: ['Option 1', 'Option 2'],
+          correctOption: 0
+        },
+      ]
+    };
+
+    const req = { body: newQuiz };
+    const res = {
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.data = data;
+      },
+    };
+
+    await createQuizWithQuestionsInternalError(req, res);
+
+    // Expectations
+    expect(res.statusCode).to.equal(500);
+    expect(res.data.message).to.equal('Internal Server Error');
+  });
 
 });
