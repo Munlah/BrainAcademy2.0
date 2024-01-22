4bf33efc095085d5f3f2a3b3d18747cbfc4abe28
MunLah <Munleng30@gmail.com>
2024-01-22 13:43:53 +0800

    New FireStore

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,7 +3,6 @@ const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  //storageBucket: 'gs://brain-academy-f877c.appspot.com', 
 });
 
 module.exports.admin = admin;
