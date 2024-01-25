8e58175247a181d77d2bf4c70a19bbda611c96ca
MunLah <Munleng30@gmail.com>
2024-01-25 12:52:47 +0800

    Updates

--- public/js/login.js
+++ public/js/login.js
@@ -1,46 +1,46 @@
 document.getElementById('loginForm').addEventListener('submit', function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-  if (!username || !password) {
-      alert('Username and password are required');
-      return;
-  }
+    if (!username || !password) {
+        alert('Username and password are required');
+        return;
+    }
 
-  fetch('http://localhost:5050/login', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-  })
-      .then(response => response.json())
-      .then(data => {
-          if (data.message === 'Login successful!') {
-              // Store user id and username in local storage
-            
-              localStorage.setItem('username', data.user.username);
-              localStorage.setItem('userId', data.user.id);
+    fetch('http://localhost:5050/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.message === 'Login successful!') {
+                // Store user id and username in local storage
 
-              // Redirect based on user role
-              if (data.user.role === 'student') {
-                  window.location.href = 'http://localhost:5050/courses.html';
-              } else if (data.user.role === 'enterprise') {
-                  window.location.href = 'http://localhost:5050/viewAllQuizzes.html';
-              }
-          } else {
-              // Show error message
-              if (data.message === 'Invalid username') {
-                  alert('Invalid username');
-              } else if (data.message === 'Invalid password') {
-                  alert('Invalid password');
-              } else {
-                  const errorElement = document.getElementById('error');
-                  errorElement.textContent = data.message;
-                  errorElement.style.display = 'block';
-              }
-          }
-      });
+                localStorage.setItem('username', data.user.username);
+                localStorage.setItem('userId', data.user.id);
+
+                // Redirect based on user role
+                if (data.user.role === 'student') {
+                    window.location.href = 'courses.html';
+                } else if (data.user.role === 'enterprise') {
+                    window.location.href = 'viewAllQuizzes.html';
+                }
+            } else {
+                // Show error message
+                if (data.message === 'Invalid username') {
+                    alert('Invalid username');
+                } else if (data.message === 'Invalid password') {
+                    alert('Invalid password');
+                } else {
+                    const errorElement = document.getElementById('error');
+                    errorElement.textContent = data.message;
+                    errorElement.style.display = 'block';
+                }
+            }
+        });
 });
