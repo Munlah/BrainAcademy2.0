d8b1548cb6f8f6c68c05d0c2348c5711d7065121
sanjana650 <sanjugundeti@gmail.com>
2024-01-25 22:13:19 +0800

    changed test file name

---
+++ tests/UIredirectfromcoursedetailtoquiz.test.js
@@ -0,0 +1,77 @@
+const { app } = require('../index');
+const { Builder, By, until } = require('selenium-webdriver');
+const { describe, it, after, before } = require('mocha');
+const { expect } = require('chai');
+const fs = require('fs').promises;
+
+//redirect to the quiz qn ui page from the course
+courseId = '2eOC6Pd7Tcx6OFqGKcPA';
+quizId = 'LK22AI2UANYHoDamMZAG';
+
+
+var server;
+before(async function () {
+  server = await new Promise((resolve) => {
+    server = app.listen(0, "localhost", () => {
+      resolve(server);
+    });
+  });
+});
+
+after(async function () {
+  await server.close();
+  process.exit(0);
+});
+
+
+describe.only('Redirect to quiz', function () {
+  this.timeout(30000);
+  let driver;
+  var counter = 0;
+
+  before(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('http://localhost:' + server.address().port + '/instrumented/courseDetails.html?courseId=2eOC6Pd7Tcx6OFqGKcPA&topic=Division');
+  });
+
+  after(async () => {
+    await driver.quit();
+  });
+
+  afterEach(async function () {
+    await driver.executeScript('return window.__coverage__;').then(async (coverageData) => {
+      if (coverageData) {
+        // Save coverage data to a file
+        await fs.writeFile('coverage-frontend/coverageValidateUserAnswer' + counter++ + '.json',
+          JSON.stringify(coverageData), (err) => {
+            if (err) {
+              console.error('Error writing coverage data:', err);
+            } else {
+              console.log('Coverage data written to coverage.json');
+            }
+          });
+      }
+    });
+  });
+
+  it('redirect to quiz when start quiz button is pressed', async function () {
+    //Wait for the start-quiz-button to be visible
+    await driver.wait(until.elementLocated(By.id('redirectQuiz')), 5000);
+
+    //Find the start-quiz-button by id
+    const startQuizButton = await driver.findElement(By.id('redirectQuiz'));
+
+    //Click the start-quiz-button
+    await startQuizButton.click();
+
+    //Wait for the redirection to complete
+    await driver.wait(until.urlContains('/validateQuiz.html?quizId=ptEFj2dtEFRDFSe1a3Rp'), 10000);
+
+
+    //Assert that the URL is redirected to the expected URL
+    const currentUrl = await driver.getCurrentUrl();
+
+    expect(currentUrl).to.include('/validateQuiz.html?quizId=ptEFj2dtEFRDFSe1a3Rp');
+  })
+
+})
