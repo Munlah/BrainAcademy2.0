8f22a000cde314902d55494f93e76b12b315d1a6
Sujitha1711 <2204733F@student.tp.edu.sg>
2024-01-27 10:24:53 +0800

    removed only

--- tests/UIviewcourseadminMicrosoft.test.js
+++ tests/UIviewcourseadminMicrosoft.test.js
@@ -19,7 +19,7 @@ after(async function () {
     process.exit(0);
 });
 
-describe.only('Testing Add and View Course in Microsoft Edge', function () {
+describe('Testing Add and View Course in Microsoft Edge', function () {
     this.timeout(30000);
     var driver; // Declare a WebDriver variable
     var counter = 0;
