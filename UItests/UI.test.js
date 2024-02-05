9e7cb8c46550c69931e233847b36f79885fa11d1
sanjana650 <75882550+sanjana650@users.noreply.github.com>
2024-02-05 11:31:19 +0800

    Update UI.test.js

--- UItests/UI.test.js
+++ UItests/UI.test.js
@@ -8,7 +8,7 @@ const { JSDOM } = require('jsdom');
 const dom = new JSDOM('<!doctype html><html><body></body></html>', { url: "http://localhost" });
 const sinon = require('sinon');
 const path = require('path');
-
+   
 global.window = dom.window;
 global.document = dom.window.document;
 global.localStorage = dom.window.localStorage;
